10c1136f6fea104763da3cdea55db26394e6aa9d
shawon111 <shawonahmmed08509@gmail.com>
2025-03-15 16:33:48 +0600

    loader color changed

--- src/app/dashboard/reminders/loading.js
+++ src/app/dashboard/reminders/loading.js
@@ -7,7 +7,7 @@ const ReminderLoader = () => {
             <Bars
                 height="80"
                 width="80"
-                color="#4fa94d"
+                color="#45dfb1"
                 ariaLabel="bars-loading"
                 wrapperStyle={{ width: "100%", textAlign: "center" }}
                 wrapperClass=""
