85c16fbc4e6a175a25651a87893458fde133b2c8
shawon111 <shawonahmmed08509@gmail.com>
2025-08-05 21:13:53 +0600

    opengraph and favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,29 @@ export const metadata = {
   description: "Your ultimate reminder app for birthdays, anniversaries, and special events. Stay organized and never miss a moment that matters!",
   keywords: ['Memory Mate', 'Reminder app', 'Birthday reminders', "Anniversary reminders", "Event reminders", "Never forget important dates", "Personal reminder app", "Timely notifications"],
   creator: 'Shawon Ahmmed',
-  publisher: 'Shawon Ahmmed'
+  publisher: 'Shawon Ahmmed',
+  openGraph: {
+    title: "Memory Mate - Your Reminder App for Birthdays, Anniversaries & More!",
+    description: "Your ultimate reminder app for birthdays, anniversaries, and special events. Stay organized and never miss a moment that matters!",
+    url: 'https://mate.fabbythemes.com',
+    siteName: 'Memory Mate',
+    images: [
+      {
+        url: 'https://mate.fabbythemes.com/memory-mate-og-img.png',
+        width: 1200,
+        height: 630,
+        alt: 'Memory Mate - Your Reminder App for Birthdays, Anniversaries & More!',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+    twitter:{
+      card: 'summary_large_image',
+      title: "Memory Mate - Your Reminder App for Birthdays, Anniversaries & More!",
+      description: "Your ultimate reminder app for birthdays, anniversaries, and special events. Stay organized and never miss a moment that matters!",
+      images: ['https://mate.fabbythemes.com/memory-mate-og-img.png']
+    }
+  },
 };
 
 // fonts
